e1180ad1d0e548c48526b2ef4316fff583c39a48
Zhuravlev1 <Zhuravlev125@gmail.com>
2018-02-27 11:23:03 +0200

    merge ios and android

--- src/pages/beacon-communication/beacon-communication.ts
+++ src/pages/beacon-communication/beacon-communication.ts
@@ -3,101 +3,114 @@ import {ToastController, ToastOptions, LoadingController, NavController} from 'i
 import {WordpressService} from "../../providers/wordpress.service";
 import {AuthenticationServiceProvider} from "../../providers/authentication-service/authentication-service";
 import {NewsPage} from "../news/news";
+import {TranslateService} from "@ngx-translate/core";
 
 
 @Component({
-    selector: 'page-beacon-communication',
-    templateUrl: 'beacon-communication.html',
+  selector: 'page-beacon-communication',
+  templateUrl: 'beacon-communication.html',
 })
 export class BeaconCommunicationPage {
-    toastOption: ToastOptions;
-    toastOptionErr: ToastOptions;
+  toastOption: ToastOptions;
+  toastOptionErr: ToastOptions;
+  beacon_translate: any;
+  lang: any;
+  companies: Array<any> = new Array<any>();
+  msg: string;
+  data_user = {
+    cookie: '',
+    nonce: ''
+  };
+  email_info = {
+    email: '',
+    subject: '',
+    message: ''
+  };
 
-    companies: Array<any> = new Array<any>();
-    msg: string;
-    data_user = {
-        cookie: '',
-        nonce: ''
-    };
-    email_info = {
-        email: '',
-        subject: '',
-        message: ''
-    };
-
-    constructor(public navCtrl: NavController,
-                public loadingCtrl: LoadingController,
-                public toast: ToastController,
-                public WPService: WordpressService,
-                public AuthService: AuthenticationServiceProvider) {
-
-        this.toastOption = {
-
-        };
-        this.toastOptionErr = {
-            message: 'Заполните пожалуйста все поля',
-            duration: 3000
-        };
-    }
-
-    ionViewDidLoad() {
-        this.AuthService.getUser()
-            .then(data => {
-                this.data_user.cookie = data.cookie;
-                this.data_user.nonce = data.nonce;
-                this.WPService.getCompanies(data.user_id, data.cookie)
-                    .subscribe((res:any) => {
-                        if (res.response.length <= 0) {
-                            console.log('no data');
-                        }else {
-                            for (let company of res.response.companies) {
-                                this.companies.push(company);
-                            }
-                        }
-                    });
-            });
-    }
-
-    send() {
-        let toastOk =  this.toast.create({
-            message: 'Сообщение успешно отправлено!',
-         //   duration: 3000,
-            showCloseButton: true,
-            closeButtonText: "Ok"
-        });
-
-        let loading = this.loadingCtrl.create({
-            spinner: 'bubbles'
-        });
-
-        this.email_info.message = this.msg;
-        if (!this.msg || !this.email_info.email || !this.email_info.subject) {
-           //let toast =  this.toast.create(this.toastOptionErr).present();
+  constructor(public navCtrl: NavController,
+              public loadingCtrl: LoadingController,
+              public toast: ToastController,
+              public translate: TranslateService,
+              public WPService: WordpressService,
+              public AuthService: AuthenticationServiceProvider) {
+    this.AuthService.getUserLang()
+      .then(res => {
+        if (!res) {
+          this.lang = 'uk';
+          this.Load();
         } else {
-            loading.present();
-            this.WPService.sendMail(this.data_user, this.email_info)
-                .subscribe((res :any)=> {
-                    if (res.response.status) {
-                        toastOk.present();
-                        toastOk.onDidDismiss((data, role) => {
-                            if (role == 'close') {
-                                this.navCtrl.setRoot(NewsPage);
-                            }
-                        });
-
-                        //this.toast.create(this.toastOption).present();
-                        loading.dismiss();
-                    }
-                });
+          this.lang = res.language;
+          this.Load();
         }
-    }
+      });
 
-    selectCompany(value) {
-        this.email_info.subject = value;
-    }
 
-    selectEmail(value) {
-        this.email_info.email = value;
+  }
+
+  Load() {
+    this.beacon_translate = this.translate.store.translations[this.lang].Beacon_communication;
+    this.AuthService.getUser()
+      .then(data => {
+        this.data_user.cookie = data.cookie;
+        this.data_user.nonce = data.nonce;
+        this.WPService.getCompanies(data.user_id, data.cookie)
+          .subscribe((res: any) => {
+            if (res.response.length <= 0) {
+              console.log('no data');
+            } else {
+              for (let company of res.response.companies) {
+                this.companies.push(company);
+              }
+            }
+          });
+      });
+  }
+
+  send() {
+    let toastErr= this.toast.create({
+      message: this.beacon_translate.send_msg_err,
+      duration: 3000
+    });
+    let toastOk = this.toast.create({
+      message: this.beacon_translate.send_msg_ok,
+      //   duration: 3000,
+      showCloseButton: true,
+      closeButtonText: "Ok"
+    });
+
+    let loading = this.loadingCtrl.create({
+      spinner: 'bubbles'
+    });
+
+    this.email_info.message = this.msg;
+    if (!this.msg || !this.email_info.email || !this.email_info.subject) {
+      toastErr.present();
+      //let toast =  this.toast.create(this.toastOptionErr).present();
+    } else {
+      loading.present();
+      this.WPService.sendMail(this.data_user, this.email_info)
+        .subscribe((res: any) => {
+          if (res.response.status) {
+            toastOk.present();
+            toastOk.onDidDismiss((data, role) => {
+              if (role == 'close') {
+                this.navCtrl.setRoot(NewsPage);
+              }
+            });
+
+            //this.toast.create(this.toastOption).present();
+            loading.dismiss();
+          }
+        });
     }
+  }
+
+  selectCompany(value) {
+    this.email_info.subject = value;
+  }
+
+  selectEmail(value) {
+    this.email_info.email = value;
+  }
 
 }
